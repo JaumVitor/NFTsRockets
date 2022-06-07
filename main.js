134e5f3f201603e46ef9b6a3023c54158d5d1a16
Jaum Vitor <joao.vitor.pires.060@gmail.com>
2022-06-07 20:34:09 -0300

    Finishing animate start .scroll and .menu-expanded

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-const container = document.querySelector('.container')
-const containerAstronauts = document.querySelector('#popular-week')
-
-nextContentSlider = event => {
-  window.onscroll = () => {
-    // window.scrollTo(event.pageX, event.pageY)
+window.onload = () => {
+  // Para verificar o scrollY sem que o evento scroll ser aplicado
+  if (window.scrollY > 0) {
+    document.body.classList.add('scroll-on')
+  } else {
+    document.body.classList.remove('scroll-on')
   }
+}
+
+window.onscroll = () => {
+  // Chama a função scrollOn toda vez que o scroll for usado
+  scrollOn()
+}
+
+function scrollOn() {
+  if (window.scrollY > 0) {
+    document.body.classList.add('scroll-on')
+  } else {
+    document.body.classList.remove('scroll-on')
+  }
+}
+// Funcões para passar imagem dos sliders
+function nextContentSlider(event) {
   if (event.deltaY < 0) {
     event.target.scrollBy(300, 0)
   } else {
@@ -12,7 +28,16 @@ nextContentSlider = event => {
   }
 }
 
+function menuExpanded() {
+  document.body.classList.toggle('menu-expanded')
+}
+
+// Declaraçõse das variaveis
+const container = document.querySelector('.container')
+const containerAstronauts = document.querySelector('#popular-week')
+const openMenu = document.querySelector('.open-menu')
+
+// Adicionando evento para os sliders, passando as funções sliders
+openMenu.addEventListener('click', menuExpanded)
 container.addEventListener('wheel', nextContentSlider)
 containerAstronauts.addEventListener('wheel', nextContentSlider)
-
-const homeSection = document.querySelector('.home-section')
